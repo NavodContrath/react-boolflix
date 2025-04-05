d97d20a81787a25957439dfec0e3da7ff93c4072
NavodContrath <navod.contrath@gmail.com>
2025-04-05 13:14:56 +0200

    refactor App component: restructure routing and context provider integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { GlobalProvider, useGlobalContext } from "./contexts/GlobalContext"
-
-
+import { GlobalProvider } from "./contexts/GlobalContext"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Layout from "./layout/Layout"
+import MoviesPage from "./components/MoviesPage"
+import Home from "./components/Home"
 function App() {
 
 
   return (
     <>
-      <GlobalProvider>
-
-      </GlobalProvider>
+      <BrowserRouter>
+        <GlobalProvider>
+          <Routes>
+            <Route Component={Layout}>
+              <Route path="/" Component={Home} />
+              <Route path="/movies" Component={MoviesPage} />
+            </Route>
+          </Routes>
+        </GlobalProvider>
+      </BrowserRouter>
     </>
   )
 }
