bf5dfbf1f4d729530e345abb121f2287ac62499e
NavodContrath <navod.contrath@gmail.com>
2025-04-07 17:18:04 +0200

    refactor: enhance Home component to include top-rated movies and series

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,34 @@
 import { useGlobalContext } from "../contexts/GlobalContext";
 import { showCards } from "../functions/utilities";
 export default function Home() {
-    const { movies, series } = useGlobalContext()
+    const { movies, series, topMovies, topSeries, searchResults } = useGlobalContext()
 
     return (
         <>
             <div className="container">
-                <div className="row align-items-md-stretch">
-                    <div className="col-lg-6 my-5">
-                        <h2 className="text-white">Movies</h2>
-                        <div className="h-100 p-5 bg-dark border rounded-3 text-white">
-                            <div className="row">
-                                {showCards(movies)}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 my-5">
-                        <h2 className="text-white">Series</h2>
-                        <div className="h-100 p-5 bg-dark border rounded-3 text-white mb-3">
-                            <div className="row">
-                                {showCards(series)}
-                            </div>
-                        </div>
+                <div className="p-5 my-4 bg-dark rounded-3">
+                    <div className="container-fluid py-5">
+                        <h1 className="display-5 fw-bold text-white">Welcome to <span className="text-danger">Netflix</span></h1>
+                        <p className="col-md-8 fs-4 text-white">
+                            Dive into the world of movies with trailers, ratings, and more.🍿
+                        </p>
+                        <p className="col-md-8 fs-4 text-white">
+                            Get started right now and enjoy your time with us!
+                        </p>
+                        <button className="btn btn-danger btn-lg mt-5" type="button">
+                            Register Now.
+                        </button>
                     </div>
                 </div>
 
+                <h2 className="text-white mt-5 mb-3">Top rated Movies</h2>
+                <div className="row">
+                    {movies && movies.length > 0 ? showCards(movies) : showCards(topMovies)}
+                </div>
+                <h2 className="text-white mt-5 mb-3">Top rated Series</h2>
+                <div className="row">
+                    {series && series.length > 0 ? showCards(series) : showCards(topSeries)}
+                </div>
             </div>
 
         </>
