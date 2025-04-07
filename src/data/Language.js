8eb5fbe72535945ae1f9161e78187905cf0aa880
NavodContrath <navod.contrath@gmail.com>
2025-04-07 04:08:12 +0200

    add Language.js: define language constants with corresponding image paths

---
+++ src/data/Language.js
@@ -0,0 +1,12 @@
+const Languages = {
+    en: "/images/us.svg",
+    it: "/images/it.svg",
+    fr: "/images/fr.svg",
+    ja: "/images/jp.svg",
+    es: "/images/es.svg",
+    zh: "/images/cn.svg",
+    de: "/images/de.svg",
+    cn: "/images/cn.svg",
+    hi: "/images/in.svg"
+}
+export default Languages
