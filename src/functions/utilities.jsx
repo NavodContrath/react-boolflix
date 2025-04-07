0fe173491f3617f9c56f2f25dca72ecfd715b591
NavodContrath <navod.contrath@gmail.com>
2025-04-07 04:26:48 +0200

    add showRating function: implement star rating display based on vote average

--- src/functions/utilities.jsx
+++ src/functions/utilities.jsx
@@ -10,4 +10,23 @@ function showFlag(item) {
             <h5 className="card-text">{item.original_language}</h5>)
     }
 }
-export default showFlag
+
+function showRating(item) {
+    const rating = [1, 2, 3, 4, 5]
+    return (
+        <>
+            {
+                rating.map((star) => (
+                    star <= Math.ceil(item.vote_average / 2) ?
+                        (<i key={star} className="fa-star me-1 fa-solid text-warning"></i>) :
+                        (<i key={star} className="fa-star me-1 fa-regular text-secondary"></i>)
+                ))
+            }
+        </>
+    )
+}
+
+
+
+
+export { showFlag, showRating }
