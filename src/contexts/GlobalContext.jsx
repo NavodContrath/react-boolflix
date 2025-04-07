d1347461d89e5065df0262a0d901c860b64089f4
NavodContrath <navod.contrath@gmail.com>
2025-04-07 17:17:58 +0200

    refactor: add top-rated movies and series fetching to GlobalContext

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -6,32 +6,39 @@ console.log(api_key)
 function GlobalProvider({ children }) {
     const [movies, setMovies] = useState([])
     const [series, setSeries] = useState([])
+    const [topMovies, setTopMovies] = useState([])
+    const [topSeries, setTopSeries] = useState([])
     const [moviesName, setMoviesName] = useState("")
     const [searchQuery, setSearchQuery] = useState("")
+    const [searchResults, setSearchResults] = useState(false)
     const moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${moviesName}`
     const seriesUrl = `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${moviesName}`
-
-    console.log(moviesName)
-
 
     useEffect(() => {
         fetch(moviesUrl)
             .then(res => res.json())
             .then(data => setMovies(data.results))
             .catch(err => console.log(err.message))
-
         fetch(seriesUrl)
             .then(res => res.json())
             .then(data => setSeries(data.results))
             .catch(err => console.log(err.message))
-
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`)
+            .then(res => res.json())
+            .then(data => setTopMovies(data.results))
+            .catch(err => console.log(err.message))
+        fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}`)
+            .then(res => res.json())
+            .then(data => setTopSeries(data.results))
+            .catch(err => console.log(err.message))
+        setSearchResults(true)
     }, [searchQuery])
-    console.log(movies)
-    console.log(series)
+    console.log(topSeries)
+    console.log(topMovies)
 
 
     return (
-        <GlobalContext.Provider value={{ movies, series, moviesName, setMoviesName, setSearchQuery }}>
+        <GlobalContext.Provider value={{ movies, series, moviesName, setMoviesName, setSearchQuery, topMovies, topSeries }}>
             {children}
         </GlobalContext.Provider>
     );
